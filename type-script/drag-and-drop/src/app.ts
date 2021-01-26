db970171c8b22a5362b496b7fd5857c9999aadba
Andrei <andrews.makarevich@gmail.com>
2021-01-26 11:45:20 +0300

    dev: init startForm
    OOP approach + TypeScript
    implemented:
    * ProjectInput class - start point - form initialization
    * Autobind Decorator
    * Fetching data from form

--- type-script/drag-and-drop/src/app.ts
+++ type-script/drag-and-drop/src/app.ts
@@ -1,1 +1,82 @@
-// Code goes here!
+function Autobind(_: any, _2: string | Symbol, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const upgradedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFunc = originalMethod.bind(this);
+      return boundFunc;
+    }
+  };
+  return upgradedDescriptor;
+}
+
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  startForm: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app') as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.startForm = importedNode.firstElementChild as HTMLFormElement;
+    this.startForm.id = 'user-input';
+
+    this.titleInputElement = this.startForm.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.startForm.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.startForm.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert('Invalid input, please try again!');
+      return
+    }
+    else {
+      return [enteredTitle, enteredDescription, +enteredPeople]
+    }
+  }
+
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;      
+      this.clearInputs();
+    }
+  }
+
+  @Autobind
+  private configure() {
+    this.startForm.addEventListener('submit', this.submitHandler)
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.startForm);
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+}
+
+const projectInput = new ProjectInput();
